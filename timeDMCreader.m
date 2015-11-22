dc4bfed4a31b9105618d335c9931fa9b34717485
michael <scienceopen@users.noreply.github.com>
2015-11-22 00:10:06 -0500

    update API

--- timeDMCreader.m
+++ timeDMCreader.m
@@ -12,43 +12,39 @@
 
 function [startframe,endframe,data] = timeDMCreader(fn,reqtime,starttime,fps,trange)
 %% parameters
-if nargin<1 || isempty(fn)
-    fn = '/media/aurora1/HST2014image/2014-03-31/2014-03-31T06-12-CamSer7196.DMCdata';
-end
-
-if nargin<2
-    reqtime = datenum([2014 3 31 7 57 0.504])*86400;
-else
+if length(reqtime)==1 %must be datenum
+elseif length(reqtime)==6 %must be datevec
     reqtime = datenum(reqtime)*86400;
-end
-
-if nargin<3
-    starttime = datenum([2014 3 31 6 12 23])*86400;
 else
-    starttime = datenum(starttime)*86400;
+    error('unknown input date request format')
 end
 
-if nargin<4
-    fps = 53;
+if length(starttime)==1 %must be datenum
+elseif length(starttime)==6 %must be datevec
+    starttime = datenum(starttime)*86400;
+else
+    error('unknown input start time format')
 end
 
 if nargin<5
     trange = [-5 20];
 end
 
-%trange
+% assumed parameters FIXME
+rowcol = [512,512];
+rcbin = [1,1];
 %% compute times chosen
 suggestedframe = round((reqtime-starttime)*fps); %this is the "raw" frame number of the .DMCdata file
 framerange = (suggestedframe+(trange(1)*fps)) : (suggestedframe+(trange(2)*fps)); % starts earlier incase timing wrong
 startframe = framerange(1);
 endframe = framerange(end);
 %% considering that our data file might be a small slice from the whole-night file, correct indices
-BytesPerImage = 512*512*16/8; %typical
+BytesPerImage = prod(rowcol)*16/8; 
 nHeadBytes = 4; %typical
-[firstRawIndex, lastRawIndex] = getRawInd(fn,BytesPerImage,nHeadBytes);
+firstRawIndex = getRawInd(fn,BytesPerImage,nHeadBytes);
 
 shiftedframerange = framerange - firstRawIndex + 1; 
 %% call DMCreader
-data = rawDMCreader(fn,512,512,1,1,shiftedframerange,0,[0,900]);
+data = rawDMCreader(fn,'rowcol',rowcol,'rcbin',rcbin,'framereq',shiftedframerange);
 
 end
