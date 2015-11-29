668a3671628e9e1330da58bc98bc36957217ae2b
Michael H <scienceopen@users.noreply.github.com>
2015-11-29 00:06:02 -0500

    filter by az,el and time

--- iridium.py
+++ iridium.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 from __future__ import division
-from six import integer_types
 from pathlib2 import Path
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
-from datetime import timedelta
 from dateutil.parser import parse
+from datetime import timedelta
 from pytz import UTC
-from numpy import s_,diff,nonzero,unique,int64,array
+from numpy import arange,diff,nonzero,array
 #
-from pymap3d.coordconv3d import eci2geodetic
+from pymap3d.coordconv3d import eci2aer,eci2geodetic
 
-def iridiumread(fn,day,tlim):
+def iridiumread(fn,day,tlim,ellim,sitella):
     fn = Path(fn).expanduser()
+    day = day.astimezone(UTC)
 #%% get all sats psuedo SV number
     with Dataset(str(fn),'r') as f:
         #psv_border = nonzero(diff(f['pseudo_sv_num'])!=0)[0] #didn't work because of consequtively reused psv #unique doesn't work because psv's can be recycled
@@ -21,49 +21,44 @@ def iridiumread(fn,day,tlim):
 # since we are only interested in one satellite at a time, why not just iterate one by one, throwing away uninteresting results
 # qualified by crossing of FOV.
 
-#%% find times in file
-    days=[day+timedelta(days=i) for i in range(len(newdayind))]
-    todayind = s_[:newdayind] #FIXME just for first day for now
-    #just one SV in this file?
-    pseudosv = unique(psv[todayind])[0]
-    assert isinstance(pseudosv,(integer_types,int64))
-
-#%% time filtering
-    dtday = day.astimezone(UTC)
-    t = array([dtday + relativedelta(hours=h) for h in time]) #TODO: need to roll day over for each 24-hour period!
-    if tlim:
-        t = t[(tlim[0] <= t) & (t <= tlim[1])]
-    ut1_unix = [d.timestamp() for d in t]
-
-#%% read rest of data
-    with Dataset(str(fn),'r') as f:
-        eci = f['pos_eci'][ind,:]
-        psv = f['pseudo_sv_num'][ind].astype(int)
+#%% consider only satellites above az,el limits for this location
+        lind = [0,0] #init
+        for i in psv_border:
+            lind = [lind[1],i]
+            cind = arange(lind[0],lind[1]-1,dtype=int) # all times for this SV
+            #now handle times for this SV
+            t = array([day + timedelta(hours=h) for h in f['time'][cind].astype(float)])
+            if tlim:
+                mask = (tlim[0] <= t) & (t <= tlim[1])
+                t = t[mask]
+                cind = cind[mask]
+            #now filter by az,el criteria
+            az,el,r = eci2aer(f['pos_eci'][cind,:],sitella[0],sitella[1],sitella[2],t)
+            if ((ellim[0] <= el) & (el <= ellim[1])).any():
+                print('sat psv {}'.format(f['pseudo_sv_num'][i]))
+                lat,lon,alt = eci2geodetic(f['pos_eci'][cind,:],t)
 
 #%%
     print(t[0].strftime('%Y-%m-%dT%H:%M:%S'))
     print(t[-1].strftime('%Y-%m-%dT%H:%M:%S'))
-    lat,lon,alt = eci2geodetic(eci,t)
+    if lon.size<200:
+        marker='.'
+    else:
+        marker=None
 
     ax = plt.figure().gca()
-    ax.plot(lon,lat)#,marker='.')
+    ax.plot(lon,lat,marker=marker)
     ax.set_ylabel('lat')
     ax.set_xlabel('long')
     ax.set_title('WGS84 vs. time')
-    ax.set_ylim((-90,90))
-    ax.set_xlim((-180,180))
+#    ax.set_ylim((-90,90))
+#    ax.set_xlim((-180,180))
     ax.grid(True)
 
     ax = plt.figure().gca()
-    ax.plot(t,alt/1e3)
+    ax.plot(t,alt/1e3,marker=marker)
     ax.set_ylabel('altitude [km]')
     ax.set_xlabel('time')
-
-    ax= plt.figure().gca()
-    ax.plot(ut1_unix)
-    ax.set_ylabel('POSIX timestamp [sec]')
-    ax.set_xlabel('index #')
-
 
     plt.show()
 
@@ -73,6 +68,8 @@ if __name__ == '__main__':
     p.add_argument('file',help='file to process')
     p.add_argument('date',help='date to process yyyy-mm-dd')
     p.add_argument('-l','--tlim',help='start stop time',nargs=2)
+    p.add_argument('-e','--ellim',help='el limits [deg]',nargs=2,type=float)
+    p.add_argument('-c','--lla',help='lat,lon,alt of site [deg,deg,meters]',nargs=3,type=float)
     p = p.parse_args()
 
     if p.tlim:
@@ -80,4 +77,4 @@ if __name__ == '__main__':
     else:
         tlim = None
 
-    iridiumread(p.file,parse(p.date+'T00Z'),tlim)
+    iridiumread(p.file,parse(p.date+'T00Z'),tlim,p.ellim,p.lla)
