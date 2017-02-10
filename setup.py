1ff005d2cc38eca496be3c78e198a63b879c3a8d
scienceopen <scienceopen@users.noreply.github.com>
2017-02-10 17:10:42 -0500

    pythonic

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
-import subprocess
 
-try:
-    import conda.cli
-    conda.cli.main('install','--file','requirements.txt')
-except Exception as e:
-    print(e)
-    import pip
-    pip.main(['install','-r','requirements.txt'])
+req = ['histutils','nose','ephem','numpy','astropy','netCDF4','matplotlib','python-dateutil','pytz']
 
 #%% install
 setup(name='satcam',
+      packages=['satcam'],
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scienceopen/satcam',
       description='intersection of camera FOV and satellite passes',
       classifiers=[
         'Programming Language :: Python :: 3.6',
       ],
-      dependency_links = [
-        'https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
-        ],
-      install_requires=[ 'histutils'
-                        ],
-      packages=['satcam']
+      install_requires=req,
       )
