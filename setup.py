62e84ed1ad2fe853d8b346eb1166774b24984ff4
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-10-22 01:51:46 -0400

    template

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
+req = ['nose','ephem','numpy','astropy','netCDF4','matplotlib','python-dateutil','pytz']
+pipreq= ['histutils']
+
+# %%
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except Exception as e:
+    pip.main(['install'] +req)
+pip.main(['install']+pipreq)
+
+# %%
 from setuptools import setup
 
-req = ['histutils','nose','ephem','numpy','astropy','netCDF4','matplotlib','python-dateutil','pytz']
-
-#%% install
 setup(name='satcam',
       packages=['satcam'],
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scivision/satcam',
       description='intersection of camera FOV and satellite passes',
       classifiers=[
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
       ],
-      install_requires=req,
+      install_requires=req+pipreq,
       )
